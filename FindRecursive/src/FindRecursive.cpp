95e2e03cda3254b689fe6ac4f6acea4f9974dd44
U-UNR\yceri <UNR+yceri@462053VWRRl1.rd.unr.edu>
2016-10-20 10:41:01 -0700

    Recursive minor changes

--- FindRecursive/src/FindRecursive.cpp
+++ FindRecursive/src/FindRecursive.cpp
@@ -13,9 +13,9 @@ int kSmall(int *arrA, int start, int end, int k);
 void swap(int *arrA, int a, int b);
 
 int main() {
-	cout << " ***** Find Smallest element Recursively ***** " << endl; // prints
+	cout << " ***** Find Smallest element Recursively ******* " << endl; // prints
 
-	int arrA[] = { 4,7,3,6,8,1,9,2 };
+	int arrA[] = { 4,7,3,6,8,1,9,2};
 	int a = 7;
 	int x = kSmall(arrA, 0, 7, a);
 
