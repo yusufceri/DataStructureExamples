fe3cced962e3a3a7d8848c8bec2363e74ebb2525
U-UNR\yceri <UNR+yceri@462053VWRRl1.rd.unr.edu>
2016-10-20 10:15:56 -0700

    Stacks

---
+++ Stacks/src/Stacks.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Stacks.cpp
+// Author      : yc
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "TEST !!!!!!! " << endl; // prints
+	return 0;
+}
